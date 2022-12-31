306a02ab222d4e25e87ef3cb9baba3a7565183be
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-12-31 22:15:09 +0900

    mainタグをコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
-import { Links } from "../components/Links";
-import { Headline } from "../components/Headline";
+import { Main } from "../components/Main";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,17 +15,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Headline
-          page="about"
-          number={1111}
-          array={[1, 2, 3]}
-          obj={{ foo: "a", bar: "bar" }}
-          boolean
-          code={<code className={styles.code}>pages/about.js</code>}
-        />
-        <Links />
-      </main>
+      <Main page="about" />
 
       <Footer />
     </>
