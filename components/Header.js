1f3c3200a5c147d29ea89e108489b6033a91bcb6
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-01-04 23:52:05 +0900

    💄 デザインを修正

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
+import classes from "./Header.module.css";
 
 export function Header() {
   return (
-    <header>
-      <Link href="/">Index</Link>
-      <Link href="/about">About</Link>
+    <header className={classes.header}>
+      <Link className={classes.anchor} href="/">
+        Index
+      </Link>
+      <Link className={classes.anchor} href="/about">
+        About
+      </Link>
     </header>
   );
 }
