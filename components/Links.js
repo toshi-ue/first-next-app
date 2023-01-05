b87cc335c58084df8b7c073e4e5fed50f9ad0946
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-01-05 21:17:00 +0900

    aタグの共通箇所を抽出、定数化

--- components/Links.js
+++ components/Links.js
@@ -1,6 +1,33 @@
 import { Inter } from "@next/font/google";
 import styles from "./Links.module.css";
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Docs <span>-&gt;</span>",
+    description:
+      "Find in-depth information about Next.js features and&nbsp;API.",
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Learn <span>-&gt;</span>",
+    description:
+      "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Templates <span>-&gt;</span>",
+    description:
+      "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy <span>-&gt;</span>",
+    description:
+      "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
+  },
+  // { href: "", title: "", description: "" },
+];
 const inter = Inter({ subsets: ["latin"] });
 
 export function Links() {
