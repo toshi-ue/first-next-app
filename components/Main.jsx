9d2f18a6b62d18bf33f3dcc14ec8279bd1ea152e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-01-06 17:13:48 +0900

    拡張子をjsからjsxに変更

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "@next/font/google";
 import classes from "./Main.module.css";
-import { Links } from "../components/Links";
-import { Headline } from "../components/Headline";
+import { Links } from "./Links";
+import { Headline } from "./Headline";
 
 const inter = Inter({ subsets: ["latin"] });
 
