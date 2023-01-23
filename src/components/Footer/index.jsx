ca92461bba045d6d21dc6f2eb22bc84d4d41bb59
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-01-23 15:01:52 +0900

    共通部分を_app.jsxに移動

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,5 @@
-// import Head from 'next/head'
 import Image from "next/image";
-// import { Inter } from '@next/font/google'
 import classes from "src/components/Footer/Footer.module.css";
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export function Footer() {
   return (
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,19 @@
 import "src/styles/globals.css";
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    // React.Fragmentはpages配下の_app.jsxなどは最上位のファイルなら他のファイルに影響を与えない（はず）なので使っても良いのではないか?
+    <>
+      <Head>
+        {/* 共通化できるものは_app.jsxにまとめてもよい */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
