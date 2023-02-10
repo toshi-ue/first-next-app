e69b85a7afe59d3bef754890037f795169b9dd0b
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-11 05:59:31 +0900

    アンマウント時の処理を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,10 +17,17 @@ export default function Home() {
   //   alert(foo);
   // }, []);
 
-  // マウント（レンダリング）されるタイミングで処理が実行される
   useEffect(() => {
+    // マウント（レンダリング）されるタイミングで処理が実行される（背景がlightblueになる）
+    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
-  });
+
+    // アンマウント時に実行される処理（背景が元に戻る）
+    console.log("アンマウント時");
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   return (
     <>
