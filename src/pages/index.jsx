d10a1f0ac5df558b676cddc55e02798954e25a2f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-13 21:34:35 +0900

    update: remove useCallback

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { Inter } from "@next/font/google";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
