dea1605f476edaf27d063f5624be705d407cbf64
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-13 21:26:16 +0900

    カウントアップを実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,16 +4,20 @@ import { Inter } from "@next/font/google";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  let foo = 1;
+  const [foo, setFoo] = useState(1);
+  // 上記の記述は以下の記述と同じ
+  // https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#%E9%85%8D%E5%88%97%E3%81%AE%E5%88%86%E5%89%B2%E4%BB%A3%E5%85%A5
+  // const foo = array[0]
+  // const setFoo = array[1]
 
   const handleClick = (e) => {
-    console.log(foo);
-    foo = foo + 1;
+    // foo = foo + 1;
+    setFoo(foo + 1);
   };
 
   useEffect(() => {
