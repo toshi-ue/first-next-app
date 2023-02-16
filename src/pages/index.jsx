e8a6fe977e79a581eee05b1048ff4a01b814a521
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-16 14:20:40 +0900

    update: useCallbackを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import { Inter } from "@next/font/google";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +15,9 @@ export default function Home() {
   // const foo = array[0]
   // const setFoo = array[1]
 
-  const handleClick = (e) => {
+  const handleClick = useCallback((e) => {
     setFoo((foo) => foo + 1);
-  };
+  }, []);
 
   useEffect(() => {
     console.log("foo");
