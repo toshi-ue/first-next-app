201ced1d9e6467c965a4e13a556251ce8b895213
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-23 07:48:16 +0900

    add: custom hook を_app.jsxに追加

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,17 @@
 import "src/styles/globals.css";
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import { useCounter } from "src/fooks/useCounter";
+import { useInputArray } from "src/fooks/useInputArray";
+import { useBackgroundLightBlue } from "src/fooks/useBackgroundLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
+  const { foo, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleAdd, handleChange } = useInputArray();
+  useBackgroundLightBlue();
+
   return (
     // React.Fragmentはpages配下の_app.jsxなどは最上位のファイルなら他のファイルに影響を与えない（はず）なので使っても良いのではないか?
     <>
